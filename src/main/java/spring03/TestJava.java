35e9a824c90598a4196231b1417bed32bcf60cd8
java <java@bigdata>
2017-09-06 16:10:25 +0900

    1. commons logging ì¶ê° 2. log4j ì¤ì 

---
+++ src/main/java/spring03/TestJava.java
@@ -0,0 +1,18 @@
+package spring03;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class TestJava {
+
+	static Log log = LogFactory.getLog(TestJava.class.getName());
+	
+	public static void main(String[] args) {
+		
+		log.info("##############################");
+		log.info("TestJava Commons logging...");
+		log.info("##############################");
+
+	}
+
+}
--- src/main/scala/com/example/TestScala.scala
+++ src/main/scala/com/example/TestScala.scala
@@ -1,8 +1,19 @@
 package com.example
 
-object TestScla {
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+object TestScala {
+	
+	var log = LogFactory.getLog(TestScala.getClass.getName)
+	
 	def main(args: Array[String]): Unit = {
 
 		println("Scala execution test...");
+		
+		log.info("##############################")
+		log.info("TestScala Commons logging...")
+		log.info("##############################")
+		
 	}
 }
