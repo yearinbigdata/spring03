31252a469f8eecebe17dd5bdaab5b693deda725c
java <java@bigdata>
2017-09-07 11:28:13 +0900

    lombok applied

--- src/main/java/com/example/spring/Dept.java
+++ src/main/java/com/example/spring/Dept.java
@@ -1,33 +1,20 @@
 package com.example.spring;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor				//필드기반 생성자 만들어줌
+@NoArgsConstructor
 public class Dept {
 
 	int deptno;
 	String dname;
 	String loc;
-
-	public int getDeptno() {
-		return deptno;
-	}
-
-	public void setDeptno(int deptno) {
-		this.deptno = deptno;
-	}
-
-	public String getDname() {
-		return dname;
-	}
-
-	public void setDname(String dname) {
-		this.dname = dname;
-	}
-
-	public String getLoc() {
-		return loc;
-	}
-
-	public void setLoc(String loc) {
-		this.loc = loc;
-	}
 
 }
